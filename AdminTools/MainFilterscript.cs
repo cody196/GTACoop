5a40040baa16c2789ecabf17374c3095907ab435
cody196 <cody.wherry@gmail.com>
2016-01-25 02:22:23 -0500

    Added SystemTime span, needs more work in progress

--- AdminTools/MainFilterscript.cs
+++ AdminTools/MainFilterscript.cs
@@ -26,6 +26,10 @@ namespace AdminTools
 
         public int ServerWeather;
         public TimeSpan ServerTime;
+        public string[] SystemTime = new[]
+        {
+            "0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23"
+        }
 
         private DateTime _lastCountdown;
 
@@ -52,7 +56,7 @@ namespace AdminTools
             Console.WriteLine("Accounts have been loaded.");
 
             ServerWeather = 0;
-            ServerTime = new TimeSpan(12, 0, 0);
+            ServerTime = new TimeSpan("SystemTime");
         }
 
         public override bool OnPlayerConnect(Client player)
